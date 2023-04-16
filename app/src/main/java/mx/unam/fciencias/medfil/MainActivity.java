553792e018d46e30e7aa2a3353793f01362838ff
hdisra318 <hdisra318@ciencias.unam.mx>
2023-04-15 22:18:14 -0600

    First Commit

---
+++ app/src/main/java/mx/unam/fciencias/medfil/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.unam.fciencias.medfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
