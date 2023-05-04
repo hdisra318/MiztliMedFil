1ddcc53b34f360c512ce539819b7ec00812279e0
Juan Manuel Lucio Rangel <juan_manuel_lr@ciencias.unam.mx>
2023-05-03 22:03:23 -0600

    Creacion del Menu de navegacion

--- app/src/main/java/mx/unam/fciencias/medfil/MainActivity.java
+++ app/src/main/java/mx/unam/fciencias/medfil/MainActivity.java
@@ -1,10 +1,30 @@
 package mx.unam.fciencias.medfil;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +36,38 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_MedFil);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menufiltros:{
+                        Toast.makeText(MainActivity.this, "Menu de filtros Seleccionado",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    case R.id.creditos:{
+                        Toast.makeText(MainActivity.this, "Creditos Seleccionado",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
     }
 }
