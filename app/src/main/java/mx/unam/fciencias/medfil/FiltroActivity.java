d94348d11ec948ef7126c7c0ac0315484b4de627
hdisra318 <hdisra318@ciencias.unam.mx>
2023-05-04 00:15:26 -0600

    Agregando cambios para iniciar con la ventana/actividad de filtros

---
+++ app/src/main/java/mx/unam/fciencias/medfil/FiltroActivity.java
@@ -0,0 +1,14 @@
+package mx.unam.fciencias.medfil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiltroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtro);
+    }
+}
