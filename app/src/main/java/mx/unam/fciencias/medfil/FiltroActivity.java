7b330b89a617878b86055f32e0b6cf621fe20660
hdisra318 <hdisra318@ciencias.unam.mx>
2023-05-07 01:13:37 -0600

    Agregando boton de captura de pantalla

--- app/src/main/java/mx/unam/fciencias/medfil/FiltroActivity.java
+++ app/src/main/java/mx/unam/fciencias/medfil/FiltroActivity.java
@@ -1,19 +1,38 @@
 package mx.unam.fciencias.medfil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.PixelCopy;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.ar.core.AugmentedFace;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.Texture;
 import com.google.ar.sceneform.ux.ArFrontFacingFragment;
 import com.google.ar.sceneform.ux.AugmentedFaceNode;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -38,6 +57,29 @@ public class FiltroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtro);
+
+        getSupportFragmentManager().addFragmentOnAttachListener(this::onAttachFragment);
+
+        if(savedInstanceState == null){
+
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction().add(R.id.vista_ra_fl_1,
+                        ArFrontFacingFragment.class, null).commit();
+            }
+        }
+
+        // Asociando el boton de captura a los metodos
+        ImageButton botonCaptura = findViewById(R.id.captura_btn);
+        botonCaptura.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                makeScreenShot();
+            }
+        });
+
+        loadTextures();
+
     }
 
     /**
@@ -56,6 +98,8 @@ public class FiltroActivity extends AppCompatActivity {
                     return null;
                 }));
 
+
+        // Para agregar mas filtros usar otro .add
     }
 
     /**
@@ -89,6 +133,102 @@ public class FiltroActivity extends AppCompatActivity {
                 nodosRostros.remove(rostroRA);
                 break;
         }
+    }
+
+    /**
+     * Metodo que responde a la creacion de la vista del fragmento RA
+     */
+    public void onViewCreated(ArSceneView arSceneView) {
+
+        vistaEscenaRa = arSceneView;
+
+        arSceneView.setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+        fragmentRostorRA.setOnAugmentedFaceUpdateListener(this::onAugmentedFaceTrackingUpdate);
+    }
+
+    /**
+     * Metodo en el que se define el comportamiento cuando se ancla el Fragmento Ar
+     * a la actividad
+     */
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+
+        if (fragment.getId() == R.id.vista_ra_fl_1) {
+
+            fragmentRostorRA = (ArFrontFacingFragment) fragment;
+            fragmentRostorRA.setOnViewCreatedListener(this::onViewCreated);
+        }
+    }
+
+    /* Liberando los recursos de la app */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        for (CompletableFuture<?> cargador : cargadores) {
+
+            if(!cargador.isDone())
+                cargador.cancel(true);
+        }
+    }
+
+    /**
+     * Realiza la captura de la vista al presionar el boton
+     */
+    private void makeScreenShot() {
+
+        final Bitmap bitmap = Bitmap.createBitmap(vistaEscenaRa.getWidth(), vistaEscenaRa.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+
+        handlerThread.start();
+        PixelCopy.request(vistaEscenaRa, bitmap, (copyResult) -> {
+
+            if (copyResult == PixelCopy.SUCCESS) {
+
+                try {
+
+                    saveBitmapLocally(bitmap);
+
+                }catch (IOException ioe) {
+
+                    Toast.makeText(FiltroActivity.this, ioe.toString(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Toast.makeText(FiltroActivity.this, "Captura guardada", Toast.LENGTH_LONG).show();
+
+            } else {
+
+                Toast.makeText(FiltroActivity.this, "No se pudo realizazr la captura", Toast.LENGTH_LONG).show();
+
+            }
+
+            handlerThread.quitSafely();
+
+        }, new Handler(handlerThread.getLooper()));
+
+
+    }
+
+    /**
+     * Metodo auxiliar que guarda el Bitmap en una ruta por defecto del dispositivo
+     */
+    private void saveBitmapLocally(Bitmap bitmap) throws IOException {
+
+        File dirDestino = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + "/arTestScreenshots");
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm.ss", Locale.getDefault());
+        String fechaFormateada =    simpleDateFormat.format(c.getTime());
+        File archivoDestino = new File(dirDestino, "capturaAR_"+fechaFormateada+".jpg");
+        Files.createDirectories(dirDestino.toPath());
+        FileOutputStream fileOutputStream = new FileOutputStream(archivoDestino);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 75, fileOutputStream);
+        fileOutputStream.flush();
+        fileOutputStream.close();
+
     }
 
 
